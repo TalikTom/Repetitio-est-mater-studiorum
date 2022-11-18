3a5663a63c369322a4e43bfa5c8182f58e434ed6
Luka Agic <lukagic@gmail.com>
2022-11-18 22:19:45 +0100

    Random greeting

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'Hello World';
+//create randomly generated greeting
+
+$randomNumber = rand(0,2);
+
+$celestialObject = ['planet', 'moon', 'star'];
+
+$greeting = "Universe";
+echo "hello {$celestialObject[$randomNumber]}";
