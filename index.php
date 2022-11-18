b91176ef9ea7e0c153815b786376c755095189f0
Luka Agic <lukagic@gmail.com>
2022-11-18 22:23:03 +0100

    Random greeting v1.2

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 //create randomly generated greeting
 
 $randomNumber = rand(0,2);
+$randomNumber2 = rand(0,2);
 
 $greeting = ['Hola', 'Hey', 'Wassup'];
 
 $celestialObject = ['planet', 'moon', 'star'];
 
 
-echo "{$greeting[$randomNumber]} {$celestialObject[$randomNumber]}";
+echo "{$greeting[$randomNumber]} {$celestialObject[$randomNumber2]}";
