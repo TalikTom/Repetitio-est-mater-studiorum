8dd2b145c81f855efbc665274747441e232341d5
Luka Agic <lukagic@gmail.com>
2022-11-19 21:46:34 +0100

    Practicing classes

--- index.php
+++ index.php
@@ -36,6 +36,30 @@ $task = [
 ];
 
 
+//class - blueprint, look for nouns eg. person, task, user, product
+
+class todoTask {
+
+    protected  $description;
+
+    protected  $completed = false;
+
+
+    public function __construct($description)
+    {
+        //current instance of the class. instance of a class is an object.
+        //decription in this case is name of the property.
+        $this->description = $description;
+
+    }
+
+    public function
+
+}
+
+$todoTask = new todoTask('Go to the store');
+
+
 
 require 'index.view.php';
 
